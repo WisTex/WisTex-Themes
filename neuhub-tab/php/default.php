a041a9d9c3b8f300a6f1de748d1d57db572e588a
Scott M. Stolz <sstolz@wistex.com>
2024-01-13 08:48:55 -0600

    Update default.php

--- neuhub-tab/php/default.php
+++ neuhub-tab/php/default.php
@@ -119,6 +119,9 @@ if (file_exists($filename)) {
         <link href="/view/theme/neuhub-tab/custom/css/codestitch.css?1685973381" rel="stylesheet"/>
         <link href="/view/theme/neuhub-tab/custom/css/codestitch-sections.css?1685973381" rel="stylesheet"/>
 
+        <link href="/view/theme/neuhub-tab/css/codestitch.css?1685973381" rel="stylesheet"/>
+        <link href="/view/theme/neuhub-tab/css/codestitch-sections.css?1685973381" rel="stylesheet"/>
+
     <style>
       /* @import url('/view/theme/neuhub-tab/assets/fonts/inter/inter.css'); */
       :root {

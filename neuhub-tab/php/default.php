def524abec9b241880a27afd17b6d355617db4d4
Scott M. Stolz <sstolz@wistex.com>
2024-01-16 10:19:18 -0600

    Update default.php

--- neuhub-tab/php/default.php
+++ neuhub-tab/php/default.php
@@ -288,17 +288,13 @@ if (file_exists($filename)) {
 
               </div>
 
-              <div class="col-lg-3 col">
-              <div id="region_1">
-              <?php if(x($page,'aside')) echo $page['aside']; ?>
-              </div>
-              </div>
 
               <div class="col-lg-7 col">
 
-              <div id="region_2">
+              <div id="!region_2">
                 <?php if(x($page,'content')) echo $page['content']; ?>          
               </div>
+
                 <!--
                 <div class="card !card-lg">
                     <div class="card-body">
@@ -340,10 +336,19 @@ if (file_exists($filename)) {
                 </div>
     -->
               </div>
-              <div class="col-lg-2 col">
 
 
-              <div id="region_3">
+
+              <div class="col-lg-3 col">
+              <div id="!region_1">
+              <?php if(x($page,'aside')) echo $page['aside']; ?>
+              </div>
+              </div>              
+
+
+              <div class="col-lg-2 col">              
+
+              <div id="!region_3">
               <?php if(x($page,'right_aside')) echo $page['right_aside']; ?>
               </div>
 

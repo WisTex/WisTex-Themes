1eade76b1c2d8372c05f700afb7624cbe0a5e2c8
Scott M. Stolz <sstolz@wistex.com>
2024-09-30 07:23:53 -0500

    test

--- redbasic-plus/php/doubleleft.php
+++ redbasic-plus/php/doubleleft.php
@@ -45,32 +45,7 @@ body {
 	<header><?php if(x($page,'header')) echo $page['header']; ?></header>
 	<?php if(x($page,'nav')) echo $page['nav']; ?>
 
-    <nav class="navbar navbar-expand-lg !sticky-bottom bg-body-tertiary">
-  <div class="container-fluid">
-    
-    <a class="navbar-brand" href="https://federated.works">Federated Works</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/siteinfo">Site Info</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Privacy</a>
-        </li>
-      </ul>
-      <span class="navbar-text">
-        Copyright &copy;2024 <a href="https://federated.works">Federated Works</a>.
-      </span>
-    </div>
 
-  </div>
-</nav>
 
 <!-- START -->
 

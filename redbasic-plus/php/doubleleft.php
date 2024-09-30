116b80cd9f3f27c7980552b20477cc386ac2a25b
Scott M. Stolz <sstolz@wistex.com>
2024-09-30 07:42:38 -0500

    test

--- redbasic-plus/php/doubleleft.php
+++ redbasic-plus/php/doubleleft.php
@@ -538,7 +538,7 @@ body {
     <footer><?php if(x($page,'footer')) echo $page['footer']; ?></footer>    
 
 <nav class="navbar navbar-expand-lg !sticky-bottom bg-body-tertiary pb-3">
-  <div class="container-flui">
+  <div class="container-fluid">
     
     <a class="navbar-brand" href="https://federated.works">Federated Works</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">

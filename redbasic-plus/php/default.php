2f748967b6729bee44c0c7ac483e516c54b1c44d
Scott M. Stolz <sstolz@wistex.com>
2024-09-30 08:02:46 -0500

    test

--- redbasic-plus/php/default.php
+++ redbasic-plus/php/default.php
@@ -534,13 +534,50 @@ body {
     </div>
   </section>
 
-	<footer><?php if(x($page,'footer')) echo $page['footer']; ?></footer>
+	
 
 
 
 
   <!-- page-content" -->
   </div>
+
+
+  <div class="fixed-bottom">
+
+    <footer><?php if(x($page,'footer')) echo $page['footer']; ?></footer>    
+
+<nav class="navbar navbar-expand-lg !sticky-bottom bg-body-tertiary">
+  <div class="container-fluid">
+    
+    <a class="navbar-brand" href="/">Expat License</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+
+    <span class="navbar-text me-auto">
+        Copyright &copy;2024 <a href="https://federated.works">Federated Works</a>.
+      </span>    
+
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/siteinfo">Site Info</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Privacy</a>
+        </li>
+      </ul>
+
+    </div>
+
+  </div>
+</nav>
+
+</div>
 
   <!--
   <nav class="navbar navbar-expand-lg sticky-bottom bg-body-tertiary">

d15d95809e06d20555e6aabe2a946e60da2cd856
Scott M. Stolz <sstolz@wistex.com>
2024-09-29 20:10:06 -0500

    Test 8

--- redbasic-plus/php/default.php
+++ redbasic-plus/php/default.php
@@ -679,7 +679,133 @@ jQuery(function ($) {
 					<div id="page-footer"></div>
 					<div id="pause"></div>
 				</section>
-				<aside id="region_3" class="d-none d-xl-block"><div class="aside_spacer_top_right"></div><div class="aside_spacer_right"><div id="right_aside_wrapper" class="aside_wrapper"><?php if(x($page,'right_aside')) echo $page['right_aside']; ?></div></div></aside>
+				<aside id="region_3" class="d-none d-xl-block"><div class="aside_spacer_top_right"></div><div class="aside_spacer_right"><div id="right_aside_wrapper" class="aside_wrapper"><?php if(x($page,'right_aside')) echo $page['right_aside']; ?>
+            
+
+                
+
+                <nav id="sidebar" class="sidebar-wrapper">
+    <div class="sidebar-content">
+      <div class="sidebar-brand">
+        <a href="#">pro sidebar</a>
+        <div id="close-sidebar"><i class="fas fa-times"></i></div>
+      </div>
+      <!-- sidebar-brand -->
+      <div class="sidebar-header">
+        <div class="user-pic" style="color:#fff;">
+          <i class="fa fa-user-circle fa-4x" aria-hidden="true"></i>
+        </div>
+        <div class="user-info">
+          <span class="user-name"> <strong>Joe Chien</strong></span>
+          <span class="user-role">Administrator</span>
+          <span class="user-status"><i class="fa fa-circle"></i> <span>Online</span></span>
+        </div>
+      </div>
+      <!-- sidebar-header -->
+      <div class="sidebar-search">
+        <div>
+          <div class="input-group">
+            <input type="text" class="form-control search-menu" placeholder="Search...">
+            <div class="input-group-append">
+              <span class="input-group-text"><i class="fa fa-search" aria-hidden="true"></i></span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- sidebar-search -->
+      <div class="sidebar-menu">
+        <ul>
+          <li class="header-menu"><span>General</span></li>
+          <li class="sidebar-dropdown">
+            <a href="#"><i class="fa fa-tachometer-alt"></i><span>Dashboard</span><span class="badge badge-pill badge-warning">New</span></a>
+            <div class="sidebar-submenu">
+              <ul>
+                <li><a href="#">Dashboard 1 <span class="badge badge-pill badge-success">Pro</span></a></li>
+                <li><a href="#">Dashboard 2</a></li>
+                <li><a href="#">Dashboard 3</a></li>
+              </ul>
+            </div>
+          </li>
+          <!-- Dashboard -->
+          <li class="sidebar-dropdown">
+            <a href="#"><i class="fa fa-shopping-cart"></i><span>E-commerce</span><span class="badge badge-pill badge-danger">3</span></a>
+            <div class="sidebar-submenu">
+              <ul>
+                <li><a href="#">Products <span>Pro</span></a></li>
+                <li><a href="#">Orders</a></li>
+                <li><a href="#">Credit cart</a></li>
+              </ul>
+            </div>
+          </li>
+          <!-- E-commerce -->
+          <li class="sidebar-dropdown">
+            <a href="#"><i class="far fa-gem"></i><span>Components</span></a>
+            <div class="sidebar-submenu">
+              <ul>
+                <li><a href="#">General</a></li>
+                <li><a href="#">Panels</a></li>
+                <li><a href="#">Tables</a></li>
+                <li><a href="#">Icons</a></li>
+                <li><a href="#">Forms</a></li>
+              </ul>
+            </div>
+          </li>
+          <!-- Components -->
+          <li class="sidebar-dropdown">
+            <a href="#"><i class="fa fa-chart-line"></i><span>Charts</span></a>
+            <div class="sidebar-submenu">
+              <ul>
+                <li><a href="#">Pie chart</a></li>
+                <li><a href="#">Line chart</a></li>
+                <li><a href="#">Bar chart</a></li>
+                <li><a href="#">Histogram</a></li>
+              </ul>
+            </div>
+          </li>
+          <!-- Charts -->
+          <li class="sidebar-dropdown">
+            <a href="#"><i class="fa fa-globe"></i><span>Maps</span></a>
+            <div class="sidebar-submenu">
+              <ul>
+                <li><a href="#">Google maps</a></li>
+                <li><a href="#">Open street map</a></li>
+              </ul>
+            </div>
+          </li>
+          <!-- Maps -->
+          
+          <li class="header-menu"><span>Extra</span></li>
+          <li><a href="#"><i class="fa fa-book"></i><span>Documentation</span><span class="badge badge-pill badge-primary">Beta</span></a></li>
+          <li><a href="#"><i class="fa fa-calendar"></i><span>Calendar</span></a></li>
+          <li><a href="#"><i class="fa fa-folder"></i><span>Examples</span></a></li>
+        </ul>
+      </div>
+      <!-- sidebar-menu  -->
+    </div>
+    <!-- sidebar-content  -->
+    <div class="sidebar-footer">
+      <a href="#">
+        <i class="fa fa-bell"></i>
+        <span class="badge badge-pill badge-warning notification">3</span>
+      </a>
+      <a href="#">
+        <i class="fa fa-envelope"></i>
+        <span class="badge badge-pill badge-success notification">7</span>
+      </a>
+      <a href="#">
+        <i class="fa fa-cog"></i>
+        <span class="badge-sonar"></span>
+      </a>
+      <a href="#">
+        <i class="fa fa-power-off"></i>
+      </a>
+    </div>
+    <!-- sidebar-footer  -->
+  </nav>
+  <!-- sidebar-wrapper  -->
+
+            
+            </div></div></aside>
 			</div>
 		</div>
 	</main>
